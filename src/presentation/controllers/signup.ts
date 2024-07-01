4ead3166ef2d5cf5dc6436a196a6587737553998
dassy <professionaldassy@gmail.com>
2024-07-01 20:39:36 -0300

    feat: ensure SignUpController return an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,9 @@
 class SignUpController {
   handle(httpRequest: any): any {
-    return { statusCode: 400 }
+    return {
+      statusCode: 400,
+      body: new Error('Missing param: name'),
+    }
   }
 }
 
